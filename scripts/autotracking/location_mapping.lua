7d628e32a2c9ea0c865dd75286e8b0eb4ed56e38
TheNickRyan <thenickolasryan@gmail.com>
2025-04-14 17:37:31 -0300

    Added boss icons to maps, modified location mapping for Rebba checks

--- scripts/autotracking/location_mapping.lua
+++ scripts/autotracking/location_mapping.lua
@@ -15,8 +15,8 @@ LOCATION_MAPPING = {
 	[BASE_LOCATION_ID + 01413] = { { "@Nexus/Urine Cartridge/" } },
 	[BASE_LOCATION_ID + 02113] = { { "@Nexus/Swamp Matcha Cartridge/" } },
 	-- Rebba Cartridge Shop
-	[BASE_LOCATION_ID + 01716] = { { "@Nexus/Rebba/Repeater/" } },
-	[BASE_LOCATION_ID + 02716] = { { "@Nexus/Rebba/Hopper/" } },
+	[BASE_LOCATION_ID + 01716] = { { "@Nexus/Repeater/" } },
+	[BASE_LOCATION_ID + 11716] = { { "@Nexus/Hopper/" } },
 
 	-- Aqueducts
 	[BASE_LOCATION_ID + 00406] = { { "@Aqueducts/West Healthkit/" } },
